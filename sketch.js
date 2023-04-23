53dc8d96a81e03eacfdb69211d5dbcad857c29b7
amanvashistha12 <av1295aman@proton.me>
2023-04-23 16:16:35 +0530

    Added a reset feature.

--- sketch.js
+++ sketch.js
@@ -1,28 +1,58 @@
 
 
 const container = document.getElementById("container");
+const pageReset = document.getElementById("reset");
+pageReset.onclick = () => resetGrid();
 
-function createGrid(rows,columns){
+function resetGrid(){
+    pageReset.addEventListener('click', ()=>{
+    container.innerHTML =" ";
+    })
+}
 
-    container.style.gridTemplateRows = `repeat(${rows}, minmax(0,1fr))`;
-    container.style.gridTemplateColumns = `repeat(${columns}, minmax(0,1fr))`;
+// Function to create Grids
+function createGrid(rows, columns){
+
+    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
         
 
-    for(let i = 0; i< rows*columns;i++){    
+    for(let i = 0; i<rows*columns;i++){    
         const squareDiv = document.createElement("div");
-        squareDiv.classList.add("square-div");
-   
+        squareDiv.classList.add("square-element");
+         
+        squareDiv.addEventListener('mouseover', function(){
+        
+            squareDiv.style.backgroundColor="black";
+        });        
 
+    
         container.appendChild(squareDiv);       
 
     }
 
 }
-createGrid(16,16);
+//Reset
+
+
+function resetGrid(){
+    container.innerHTML = " ";
+    createGrid(16,16);
+}
+resetGrid();
+
+
+
+
+
+
+
+
 
 
 window.onload = function() {
-    createGrid();
+ 
+
   };
 
 
